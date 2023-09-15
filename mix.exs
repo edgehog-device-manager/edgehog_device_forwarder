9b2423935aa90ab606b19d52fa16bbe7adcedfcf
Francesco Noacco <francesco.noacco@secomind.com>
2023-09-15 10:54:49 +0200

    chore: add con_cache dependency
    ConCache seem to be the most adequate solution to the problem,
    as it is mostly an ETS wrapper which allows for
    atomic row-based operations and is designed for parallelism.
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule EdgehogDeviceForwarder.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:con_cache, "~> 1.0"}
     ]
   end
 
