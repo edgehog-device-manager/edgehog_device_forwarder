1b654abdba32e1789bf796bb66f6194d7db7f276
Francesco Noacco <francesco.noacco@secomind.com>
2023-10-02 17:28:22 +0200

    feat(user_controller): init
    use the forwarder api to forward and receive the device response
    
    future PRs for the controller will need to
    - handle websocket upgrade (done in #10)
    - have a fallback controller of some sort (done in #16)
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- lib/edgehog_device_forwarder_web/endpoint.ex
+++ lib/edgehog_device_forwarder_web/endpoint.ex
@@ -30,10 +30,7 @@ defmodule EdgehogDeviceForwarderWeb.Endpoint do
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-  plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
-    pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
+  plug EdgehogDeviceForwarderWeb.BodyParser
 
   plug Plug.MethodOverride
   plug Plug.Head
