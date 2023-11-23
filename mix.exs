85528d9c133ec1d10dc5c2aee5d353a4082515c3
Francesco Noacco <francesco.noacco@secomind.com>
2023-11-23 17:07:44 +0100

    style: dependencies in alphabetical order
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -28,13 +28,13 @@ defmodule EdgehogDeviceForwarder.MixProject do
 
   defp deps do
     [
-      {:phoenix, "~> 1.7.7"},
-      {:phoenix_live_dashboard, "~> 0.8.0"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:phoenix, "~> 1.7.7"},
+      {:phoenix_live_dashboard, "~> 0.8.0"},
+      {:plug_cowboy, "~> 2.5"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
