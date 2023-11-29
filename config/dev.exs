a2244bccb349ea232bfaea3921c186b65e090ec0
Francesco Noacco <francesco.noacco@secomind.com>
2023-11-29 16:29:55 +0100

    feat: accept http connections form everywhere in dev
    Since the scope of this project is redirecting traffic between devices,
    it makes sense that even in a dev environment we allow connections
    from remote devices.
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@
 import Config
 
 config :edgehog_device_forwarder, EdgehogDeviceForwarderWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
